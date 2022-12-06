5c80800f83239f533a8d0db2368fe2cab5e19bc7
Vadim V P <Everestxxx555@yandex.ru>
2022-12-07 01:16:51 +0300

    коммит изменен

---
+++ Seminar_C_2/ZD _1/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+Console.Write("введите число ");
+int number = int.Parse(Console.ReadLine());
+
+//if ((number % 7 == 0) && (number % 23 == 0))
+
+if (number % 7 == 0 && number % 23 == 0)
+{
+    Console.Write($"{number} -> кратно 7 и 23");
+}
+else
+{
+    Console.Write($"{number} -> не кратно 7 и 23");
+}
