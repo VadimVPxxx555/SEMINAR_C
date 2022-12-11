6cfc7cee369319081e7b5f0ad5f61a1ec9903760
Vadim V P <Everestxxx555@yandex.ru>
2022-12-11 19:17:03 +0300

    коммит C3 ZD 1

---
+++ Seminar_C_3/ZD _1/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Clear();
+int DataEntruXY(string str)
+{
+   Console.Write(str);
+   int number = int.Parse(Console.ReadLine());
+   return number;
+}
+void FindQvadrant(int x, int y)
+{
+    if ((x > 0) && (y > 0)) Console.WriteLine("это первый квадрант");
+    if ((x < 0) && (y > 0)) Console.WriteLine("это второрй квадрант");
+    if ((x < 0) && (y < 0)) Console.WriteLine("это третий квадрант");
+    if ((x > 0) && (y < 0)) Console.WriteLine("это четвертый квадрант");
+}
+
+int x = DataEntruXY("введите координаты x = ");
+
+int y = DataEntruXY("введите координаты y = ");
+
+FindQvadrant(x, y);
