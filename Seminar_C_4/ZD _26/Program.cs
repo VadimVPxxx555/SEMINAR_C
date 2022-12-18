d076d604b544c471cc931d608eaf375528758c2a
Vadim V P <Everestxxx555@yandex.ru>
2022-12-18 18:51:09 +0300

    коммит ZD _26

---
+++ Seminar_C_4/ZD _26/Program.cs
@@ -0,0 +1,26 @@
+﻿// Напишите программу, которая принимает на вход число 
+// и выдает количество цифр в числе.
+Console.Clear();
+int DataSchet(string str)
+{
+    Console.Write(str);
+    int number = int.Parse(Console.ReadLine());
+    return number;
+}
+
+int Schetnumb(int numberSchet)
+{
+  //int nunb = numberSchet;
+  int i = 0;
+  for(i = 0; numberSchet > 0; i++)
+  {
+    numberSchet = numberSchet / 10;
+  }
+  return i;
+}
+
+int numberSchet = DataSchet("Введите число ");
+
+int i = Schetnumb(numberSchet);
+
+Console.WriteLine(i);
